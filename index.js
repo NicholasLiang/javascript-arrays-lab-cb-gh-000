b3d43bc26fb65c9668a89266b89dd35c0b2ea660
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 14:59:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var destructivelyAppendKitten=(name)=>{kittens.push(name);return kittens;}
 var destructivelyPrependKitten=(name)=>{kittens.unshift(name);return kittens;}
 var destructivelyRemoveLastKitten=()=>{kittens.pop();return kittens;}
 var destructivelyRemoveFirstKitten=()=>{kittens.shift();return kittens;}
-var appendKitten=(name)=>[kittens, name]
-var prependKitten=(name)=>[name, kittens]
+var appendKitten=(name)=>[...kittens, name]
+var prependKitten=(name)=>[name, ...kittens]
 
 
 
