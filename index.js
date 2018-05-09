4cc4b3708a79df3cf1093bd22cc1776b1dbebfab
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 14:56:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 var destructivelyAppendKitten=(name)=>{kittens.push(name);return kittens;}
-var destructivelyPrependKitten=(name)=>{}
+var destructivelyPrependKitten=(name)=>{kittens.}
 
 
 //   describe('destructivelyPrependKitten(name)', function() {
