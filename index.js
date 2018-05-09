c6c3e307d750f84ec65ea32483a357cfb07b068e
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 15:00:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,22 +8,3 @@ var appendKitten=(name)=>[...kittens, name]
 var prependKitten=(name)=>[name, ...kittens]
 var removeLastKitten=()=>kittens.slice(0,kittens.length-1);
 var removeFirstKitten=()=>kittens.slice(1);
-
-
-//   describe('removeLastKitten()', function() {
-//     it('removes the last kitten in the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-//       expect(removeLastKitten()).toEqual(["Milo", "Otis"])
-
-//       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-//     })
-//   })
-
-//   describe('removeFirstKitten()', function() {
-//     it('removes the first kitten from the kittens array and returns a new array, leaving the kittens array unchanged', function() {
-//       expect(removeFirstKitten()).toEqual(["Otis", "Garfield"])
-
-//       expect(window.kittens).toEqual(["Milo", "Otis", "Garfield"])
-//     })
-//   })
-
-// })
